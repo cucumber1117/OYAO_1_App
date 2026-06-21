e61cd5844914f319a842997591dc7b4aff1670e3
cucumber1117 <masaomi4869@gmail.com>
2026-06-21 15:00:57 +0900

    コンフリクト修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<Home />} />
-		</Routes>
-	);
+	return <Home />;
 }
 
 export default App;
--- src/components/Roulette/Roulette.jsx
+++ src/components/Roulette/Roulette.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './Roulette.css';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { buildWheelGradient, getResultRotation, spinResult } from '../../utils/rouletteLogic';
@@ -78,36 +77,3 @@ export default function Roulette({ items = [], onResult = () => {}, onSpin = nul
 		</div>
 	);
 }
-=======
-import './Home.css';
-import ItemEditor from '../../components/ItemEditor/ItemEditor';
-import Roulette from '../../components/Roulette/Roulette';
-import ResultDisplay from '../../components/ResultDisplay/ResultDisplay';
-
-export default function Home() {
-  return (
-    <div className="home">
-      <header className="home__header">
-        <h1>ルーレットアプリ</h1>
-      </header>
-
-      <main className="home__main">
-        {/* 左カラム：項目編集 */}
-        <section className="home__panel home__panel--left">
-          <ItemEditor />
-        </section>
-
-        {/* 中央カラム：ルーレット本体 */}
-        <section className="home__panel home__panel--center">
-          <Roulette />
-        </section>
-
-        {/* 右カラム：結果表示 */}
-        <section className="home__panel home__panel--right">
-          <ResultDisplay />
-        </section>
-      </main>
-    </div>
-  );
-}
->>>>>>> 3e691da0c3939d2e17c99cbce0068a24df056c8a
